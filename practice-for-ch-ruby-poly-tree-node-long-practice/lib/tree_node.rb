44bc1215dd08dc06e4db93fe2235684d05dfb93c
ningxiao <dsgrichard@hotmail.com>
2023-02-17 14:24:53 -0800

    First code

--- practice-for-ch-ruby-poly-tree-node-long-practice/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice/lib/tree_node.rb
@@ -1,3 +1,22 @@
 class PolyTreeNode
-  test
+    attr_reader :parent, :children, :value
+  
+    def initialize (value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent= (parent_node)
+        if parent_node == nil 
+            
+        @parent = parent_node
+        parent_node.children << self if !parent_node.children.include?(self)
+
+    end
+
+
+    def inspect
+        @value.inspect
+    end
 end
