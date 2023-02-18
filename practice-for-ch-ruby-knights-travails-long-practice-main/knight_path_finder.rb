83cdaaddc3efd05b2dfa10177a55023d2469eb5e
Kathy Vu <vu.kathy98@gmail.com>
2023-02-17 17:32:32 -0800

    Add tree_node file, #valid_moves

--- practice-for-ch-ruby-knights-travails-long-practice-main/knight_path_finder.rb
+++ practice-for-ch-ruby-knights-travails-long-practice-main/knight_path_finder.rb
@@ -1,5 +1,27 @@
+require_relative "tree_node"
 class KnightPathFinder
+
     def initialize(start_pos)
+        @root_node = PolyTreeNode.new(start_pos)
+        @considered_positions = [start_pos]
     end
 
+    def self.valid_moves(pos) #8 possible moves
+        x, y = pos
+        possible_moves = []
+        [-2, -1, 1, 2].each do |i|
+            [-2, -1, 1, 2].each do |j|
+                possible_moves << [x+i, y+j] if i.abs != j.abs
+            end
+        end
+        possible_moves
+    end
+
+    def build_move_tree #use new_move_pos, build in breadth first manner
+
+    end
+
+    def new_move_positions(pos) #call ::valid_moves method
+        
+    end
 end
